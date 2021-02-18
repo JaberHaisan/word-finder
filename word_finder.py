6b3e6232e21fe901679c41a4f8106204efb62ac1
Jaber Haisan <jaberhaisan@gmail.com>
2021-02-19 00:33:57 +0600

    Update word_finder.py

--- word_finder.py
+++ word_finder.py
@@ -10,19 +10,28 @@ with open("words_alpha.txt") as f_obj:
 letters = input("Letters which should be present: ")
 maximum_length = int(input("Maximum length of word: "))
 
+# Remove non unique letters.
+letters = set(letters)
+
 # Keep a gap between input and output.
 print()
 
+# Find all words that match up to maximum length.
 for length in range(1, maximum_length + 1):
+	
 	# Get all possible combinations from letters equal to length.
 	all_combinations = itertools.product(letters, repeat=length)
 
 	matches = []
-	for perm in all_combinations:
-		word = "".join(perm)
-		if word in words:
-			matches.append(word)
+	for comb in all_combinations:
+		joined_comb = "".join(comb)
+		if joined_comb in words:
+			matches.append(joined_comb)
 	
 	print("{}: ".format(length), end="")
-	print(*matches, sep=", ", end="\n")
+	
+	if matches:
+		print(*matches, sep=", ")
+	else:
+		print("N/A")
 	print()
